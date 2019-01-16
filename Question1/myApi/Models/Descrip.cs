e2dbb845aa96bbcadc525389bb68dea983da732b
Kritsana <tragoolphrom@gmail.com>
2019-01-16 11:46:05 +0700

    Q1 Done

---
+++ Question1/myApi/Models/Descrip.cs
@@ -0,0 +1,10 @@
+namespace myApi.Models
+{
+    public class Descript
+    {
+        public int Year { get; set; }
+        public double Balance { get; set; }
+        public double Interest { get; set; }
+        public double Payments { get; set; }
+    }
+}
